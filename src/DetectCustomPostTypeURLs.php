2fedfbea980fb149326d322b3692973f5ea0774e
Zsombor Paróczi <zsombor.paroczi@gmail.com>
2021-03-19 18:17:53 +0100

    Exclude all non-public types

--- src/DetectCustomPostTypeURLs.php
+++ src/DetectCustomPostTypeURLs.php
@@ -12,13 +12,26 @@ class DetectCustomPostTypeURLs {
     public static function detect( string $wp_site_url ) : array {
         global $wpdb;
 
-        $post_urls = [];
+        // get "non-public" post types
+        $non_public_post_types = array_merge(
+            get_post_types(array(
+                'public' => false,
+                '_builtin' => true
+            ), 'names'),
+            get_post_types(array(
+                'public' => false,
+                '_builtin' => false
+            ), 'names'),
+            array('revision', 'nav_menu_item'));
 
+
+        $post_urls = [];
+        // TODO: $non_public_post_types escape
         $post_ids = $wpdb->get_col(
             "SELECT ID
             FROM {$wpdb->posts}
             WHERE post_status = 'publish'
-            AND post_type NOT IN ('revision','nav_menu_item')"
+            AND post_type NOT IN ('".implode("','",$non_public_post_types)."')"
         );
 
         foreach ( $post_ids as $post_id ) {
