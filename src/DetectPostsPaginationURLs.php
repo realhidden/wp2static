3d04853b1442d615204a6546ff1704dccf4061d1
Zsombor Paróczi <zsombor.paroczi@gmail.com>
2021-04-12 13:30:58 +0200

    Custom post type archive detector

--- src/DetectPostsPaginationURLs.php
+++ src/DetectPostsPaginationURLs.php
@@ -12,6 +12,9 @@ class DetectPostsPaginationURLs {
     public static function detect( string $wp_site_url, string $wp_home_url ) : array {
         global $wpdb, $wp_rewrite;
 
+        // remove trailing slash from site_url
+        $site_url_notrail = strlen($wp_site_url) > 0 ? substr($wp_site_url, 0, strlen($wp_site_url) - 1) : $wp_site_url;
+
         // get "non-public" post types
         $non_public_post_types = array_merge(
             get_post_types(array(
@@ -24,99 +27,68 @@ class DetectPostsPaginationURLs {
             ), 'names'),
             array('revision', 'nav_menu_item'));
 
-        $post_urls = [];
-        $unique_post_types = [];
-
-        $query = "
-            SELECT ID,post_type
-            FROM %s
-            WHERE post_status = '%s'
-            AND post_type NOT IN ('".implode("','",$non_public_post_types)."')";
-
         $posts = $wpdb->get_results(
-            sprintf(
-                $query,
+            sprintf("SELECT post_type, COUNT(id) as count FROM %s WHERE post_status = '%s'
+            AND post_type NOT IN ('" . implode("','", $non_public_post_types) . "')
+            GROUP BY post_type",
                 $wpdb->posts,
                 'publish'
             )
         );
 
-        foreach ( $posts as $post ) {
+        // grab currently registered unique post types
+        $public_post_types = array_merge(
+            get_post_types(array(
+                'public' => true,
+                '_builtin' => true
+            ), 'names'),
+            get_post_types(array(
+                'public' => true,
+                '_builtin' => false
+            ), 'names'));
+
+        // also grab the types for DB
+        $postcount = [];
+        foreach ($posts as $post) {
             // capture all post types
-            $unique_post_types[] = $post->post_type;
+            $public_post_types[] = $post->post_type;
+            $postcount[$post->post_type] = $post->count;
         }
 
         // get all pagination links for each post_type
-        $post_types = array_unique( $unique_post_types );
+        $public_post_types = array_unique($public_post_types);
         $pagination_base = $wp_rewrite->pagination_base;
-        $default_posts_per_page = get_option( 'posts_per_page' );
+        $default_posts_per_page = get_option('posts_per_page');
 
         $urls_to_include = [];
+        foreach ($public_post_types as $post_type) {
+            $post_type_obj = get_post_type_object($post_type);
+            $post_type_total = isset($postcount[$post_type]) ? (int)$postcount[$post_type] : 0;
 
-        foreach ( $post_types as $post_type ) {
-            $query = "SELECT COUNT(*) FROM %s WHERE post_status = '%s'" .
-                " AND post_type = '%s'";
-
-            $post_type_total = $wpdb->get_var(
-                sprintf(
-                    $query,
-                    $wpdb->posts,
-                    'publish',
-                    $post_type
-                )
-            );
-
-            if ( ! $post_type_total ) {
+            // cannot find the post object, maybe it was deleted (but still in DB)
+            if (!$post_type_obj) {
                 continue;
             }
 
-            $post_type_obj = get_post_type_object( $post_type );
-
-            if ( ! $post_type_obj ) {
+            $post_archive_link = get_post_type_archive_link($post_type);
+            // post_type_archive link returns false if there is no archive page
+            if ($post_archive_link === false) {
                 continue;
             }
-
-            // cast WP's object back to array
-            $post_type_labels = (array) $post_type_obj->labels;
-
-            $plural_form = strtolower( $post_type_labels['name'] );
-
-            // skip post type names containing spaces
-            if ( strpos( $plural_form, ' ' ) !== false ) {
-                continue;
-            }
-
-            $total_pages = ceil( $post_type_total / $default_posts_per_page );
-
-            for ( $page = 1; $page <= $total_pages; $page++ ) {
-                // TODO: skipping page pagination here, but is it covered elsewhere?
-                if ( $post_type === 'page' ) {
-                    continue;
-                }
-
-                if ( $post_type === 'post' ) {
-                    $post_archive_slug = '';
-
-                    // check if a Posts page has been set in Settings > Reading
-                    if ( get_option( 'page_for_posts' ) !== '0' ) {
-
-                        // get FQURL to Posts Page
-                        $post_archive_link = get_post_type_archive_link( 'post' );
-
-                        if ( $post_archive_link ) {
-                            $post_archive_slug = str_replace(
-                                $wp_site_url,
-                                '',
-                                trailingslashit( $post_archive_link )
-                            );
-                        }
-                    }
-
-                    $urls_to_include[] = "/" . str_replace($wp_site_url, "", $wp_home_url) . "{$post_archive_slug}{$pagination_base}/{$page}/";
+            $total_pages = ceil($post_type_total / $default_posts_per_page);
+            for ($page = 0; $page <= $total_pages; $page++) {
+                $newurl = '';
+                if ($page === 0) {
+                    $newurl = str_replace($site_url_notrail, "", $post_archive_link);
                 } else {
-                    $urls_to_include[] =
-                        "/" . str_replace($wp_site_url, "", $wp_home_url) . "{$plural_form}/{$pagination_base}/{$page}/";
+                    $newurl = str_replace($site_url_notrail, "", $post_archive_link) . "{$pagination_base}/{$page}/";
                 }
+
+                // Make sure url starts with "/"
+                if (strlen($newurl) === 0 || $newurl[0] !== '/') {
+                    $newurl = "/" . $newurl;
+                }
+                $urls_to_include[] = $newurl;
             }
         }
 
